37770b061f4559ec9768d8c04e5e031e78742165
jddingemanse <86903675+jddingemanse@users.noreply.github.com>
2022-06-18 11:45:42 +0300

    Add files via upload

--- src/pycamtETinterface/__init__.py
+++ src/pycamtETinterface/__init__.py
@@ -1,32 +1,3 @@
-__all__ = ['dataFunctions','plotFunctions','mapFunctions']
+__all__ = ['interface']
 
-from .pckgSettings import initSettings as _initSettings
-setDict = _initSettings(str(__file__))
-
-from . import dataFunctions
-from . import plotFunctions
-
-import importlib as _importlib
-import pathlib as _pathlib
-
-_gpd_spec = _importlib.util.find_spec("geopandas")
-if _gpd_spec is None:
-    print('Package geopandas is not installed. example_package.mapFunctions cannot be used.')
-else:
-    if _pathlib.Path(setDict['adm3Path']).exists()==False:
-        print('File location of adm3 (ET districts) shape file not found at '+setDict['adm3Path']+'. Map abilities cannot be used.\n',
-              'If you have the shape file, put it at that location, or set the full path by using pckgSettings.setSettings(adm3Path="path/to/adm3shapefiles")')
-    else:
-        if _pathlib.Path(setDict['adm2Path']).exists()==False:
-            print('File location of adm2 (ET zones) shape file not found at '+setDict['adm2Path']+'. Map abilities cannot be used.\n',
-                  'If you have the shape file, put it at that location, or set the full path by using pckgSettings.setSettings(adm2Path="path/to/adm2shapefiles")')
-        else:
-            if _pathlib.Path(setDict['adm1Path']).exists()==False:
-                print('File location of adm1 (ET regions) shape file not found at '+setDict['adm1Path']+'. Map abilities cannot be used.\n',
-                      'If you have the shape file, put it at that location, or set the full path by using pckgSettings.setSettings(adm1Path="path/to/adm1shapefiles")')
-            else:
-                if _pathlib.Path(setDict['adm0Path']).exists()==False:
-                    print('File location of adm0 (full Ethiopia) shape file not found at '+setDict['adm0Path']+'. Map abilities cannot be used.\n',
-                          'If you have the shape file, put it at that location, or set the full path by using pckgSettings.setSettings(adm0Path="path/to/adm0shapefiles")')
-                else:
-                    from . import mapFunctions
+from . import interface
